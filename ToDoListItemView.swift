129e76e79585cd77d2df1e90c3c89ac500518907
_/ <145014365+testuroo@users.noreply.github.com>
2023-09-20 07:08:08 +0900

    Add files via upload

---
+++ ToDoListItemView.swift
@@ -0,0 +1,45 @@
+//
+//  ToDoListItemsView.swift
+//  ToDoListApp
+//
+//  Created by Tetsurou Morishita on 2023/09/12.
+//
+
+import SwiftUI
+
+struct ToDoListItemView: View {
+    let item: ToDoListItem
+    
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text(item.title)
+                    .font(.title)
+
+                Text("\(Date(timeIntervalSince1970: item.duedate).formatted(date: .abbreviated, time: .shortened))")
+                    .font(.footnote)
+                    .foregroundColor(Color(.secondaryLabel))
+                }
+            Spacer()
+            
+            Button {
+                
+            } label: {
+                Image(systemName: item.isDone ? "circle.chekmark.fill" : "cirle")
+            }
+            
+        }
+    }
+}
+
+struct ToDoListItemsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ToDoListItemView(item: .init(
+            id: "123",
+            title: "Get milk",
+            duedate: Date().timeIntervalSince1970,
+            createdDate: Date().timeIntervalSince1970,
+            isDone: true
+        ))
+    }
+}
---
+++ ToDoListView.swift
@@ -0,0 +1,63 @@
+//
+//  ToDoListView.swift
+//  ToDoListApp
+//
+//  Created by Tetsurou Morishita on 2023/09/12.
+//
+import FirebaseFirestoreSwift
+import SwiftUI
+
+struct ToDoListView: View {
+    @StateObject var viewModel = ToDoListViewViewModel()
+    @FirestoreQuery var items: [ToDoListItem]
+    //private let userId: String
+    
+    
+    init(userId: String) {
+        //self.userId = userId
+        ///userid <id>/todo/<entries>
+        self._items = FirestoreQuery(
+            collectionPath: "users/\(userId)/todos"
+        )
+    }
+    var body: some View {
+        NavigationView{
+            VStack {
+                List(items) { item in
+                    //Text(item.title)
+                    ToDoListItemView(item: item)
+                        .swipeActions {
+                            Button {
+                                //Delete
+                            } label: {
+                                Text("delete")
+                                    .foregroundColor(Color.red)
+                            }
+
+                        }
+                }
+                .listStyle(PlainListStyle())
+            }
+            .navigationTitle("To Do List")
+            .toolbar {
+                Button {
+                    //Action
+                    viewModel.showingNewItemView = true
+                } label: {
+                    Image(systemName: "plus")
+                }
+
+            }
+            .sheet(isPresented: $viewModel.showingNewItemView) {
+                NewItemView(newItemPresented: $viewModel.showingNewItemView)
+            }
+            
+        }
+    }
+}
+
+struct ToDoListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ToDoListView(userId: "")
+    }
+}
